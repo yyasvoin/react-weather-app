5fb9b000fd82f6fe8edec64e19f2ed4956d2a204
Yuri Yasvoin <yyasvoin@equinix.com>
2021-10-29 21:59:49 -0400

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 // jshint esversion:9
 /* jshint node: true */
 /* jshint browser: true */
-import axios from 'axios';
-require('dotenv').config();
+import axios from "axios";
+require("dotenv").config();
 
 console.log(process.env);
 
-const api_key = 'bcdb5c00e22a7f46b78eb5f578591e39';
-const url = 'https://api.openweathermap.org/data/2.5/weather';
+const api_key = process.env.API_KEY;
+const url = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeather = async (city, country) => {
 	const data = await axios.get(
